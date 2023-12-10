3e943939a65fe640cff6f11de6b3105aef1b3e1c
Арман <@ARMAN>
2023-12-10 23:43:12 +0600

    Добавьте файлы проекта.

---
+++ prac15(2)/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Type myClassType = typeof(MyClass);
+
+        // Создание экземпляра MyClass без прямого вызова конструктора
+        object myClassInstance = Activator.CreateInstance(myClassType);
+
+        Console.WriteLine("Экземпляр MyClass создан.");
+    }
+}
